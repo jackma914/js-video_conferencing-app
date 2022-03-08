ac1e769e4ff9ce9abe0cdc1a702bd8094a22caad
joon <83916101+jackma914@users.noreply.github.com>
2022-03-08 21:25:56 +0900

    ADD create view
    나를 보여주는 view 생성

--- public/script.js
+++ public/script.js
@@ -1,5 +1,15 @@
+const videoGrid = document.getElementById("video-grid");
+
+//HTML 문서에서, Document.createElement() 메서드는 지정한 tagName의 HTML 요소를 만들어 반환합니다.
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+
 let myVideoStream;
 
+//1. navigator.mediaDevices
+//  읽기 전용 속성은 카메라, 마이크, 화면 공유와 같이 현재 연결된 미디어 입력 장치에 접근할 수 있는 MediaDevices 객체를 반환합니다.
+//2. MediaDevices 인터페이스의 getUserMedia() 메서드는 사용자에게 미디어 입력 장치 사용 권한을 요청하며,
+// 사용자가 수락하면 요청한 미디어 종류의 트랙을 포함한 MediaStream(en - US)을 반환합니다.
 navigator.mediaDevices
   .getUserMedia({
     video: true,
@@ -7,11 +17,18 @@ navigator.mediaDevices
   })
   .then((stream) => {
     myVideoStream = stream;
+    console.log(stream);
+    addVideoStream(myVideo, stream);
   });
 
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
+
+  //   미디어의 메타 데이터가 로드되었을 때를 나타낸다.
+  // 메타 데이터는 우리가 유용하게 사용할 수 있는 동영상의 재생시간과 같은 것을 의미한다.
+  // 미디어가 로드되기 전에, 먼저 메타 데이터를 뽑아와서 활용할 수 있다.
   video.addEventListener("loadedmetadata", () => {
     video.play();
   });
+  videoGrid.append(video);
 };
