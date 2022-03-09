cb7ceb7ef144e2ee3e8261b89fe03c92f44fd019
joon <jackma914@gmail.com>
2022-03-09 21:32:23 +0900

    ADD
    디버깅이 어려워서 처음부터 다시 구현해보겠습니다.

--- public/script.js
+++ public/script.js
@@ -1,49 +1,58 @@
 const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
-
-//HTML 문서에서, Document.createElement() 메서드는 지정한 tagName의 HTML 요소를 만들어 반환합니다.
+const myPeer = new Peer(undefined, {
+  host: "/",
+  port: "3001",
+});
 const myVideo = document.createElement("video");
 myVideo.muted = true;
-
-let myVideoStream;
-
-//1. navigator.mediaDevices
-//  읽기 전용 속성은 카메라, 마이크, 화면 공유와 같이 현재 연결된 미디어 입력 장치에 접근할 수 있는 MediaDevices 객체를 반환합니다.
-//2. MediaDevices 인터페이스의 getUserMedia() 메서드는 사용자에게 미디어 입력 장치 사용 권한을 요청하며,
-// 사용자가 수락하면 요청한 미디어 종류의 트랙을 포함한 MediaStream(en - US)을 반환합니다.
+const peers = {};
 navigator.mediaDevices
   .getUserMedia({
     video: true,
     audio: true,
   })
   .then((stream) => {
-    myVideoStream = stream;
-    //받아온 MediaStream을 myVideoStream에 넣어주고, addVideoStream 함수에 인자로 myVicdoe와 MediaStream값을 보내줍니다.
     addVideoStream(myVideo, stream);
+
+    myPeer.on("call", (call) => {
+      call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    socket.on("user-connected", (userId) => {
+      connectToNewUser(userId, stream);
+    });
   });
 
-socket.emit("join-room", ROOM_ID);
-
-socket.on("user-connected", () => {
-  connecToNewUser();
+socket.on("user-disconnected", (userId) => {
+  if (peers[userId]) peers[userId].close();
 });
 
-const connecToNewUser = () => {
-  console.log("new user");
-};
+myPeer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+});
 
-const addVideoStream = (video, stream) => {
-  // !important 받아온 stream을 video.srcObject에 넣어주면 실시간으로 영상을 볼수 있습니다.
-  video.srcObject = stream;
-
-  //   미디어의 메타 데이터가 로드되었을 때를 나타낸다.
-  // 메타 데이터는 우리가 유용하게 사용할 수 있는 동영상의 재생시간과 같은 것을 의미한다.
-  // 미디어가 로드되기 전에, 먼저 메타 데이터를 뽑아와서 활용할 수 있다.
-  video.addEventListener("loadedmetadata", () => {
-    //이벤트가 작동하면 video를 싱행합니다.
-    video.play();
+function connectToNewUser(userId, stream) {
+  const call = myPeer.call(userId, stream);
+  const video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+  call.on("close", () => {
+    video.remove();
   });
 
-  // append를 이용해 video를 element를 추가해줍니다.
+  peers[userId] = call;
+}
+
+function addVideoStream(video, stream) {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
   videoGrid.append(video);
-};
+}
--- server.js
+++ server.js
@@ -2,34 +2,28 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
-const { v4: uuidv4 } = require("uuid");
+const { v4: uuidV4 } = require("uuid");
 
-//정적으로 public폴더의 파일을 불러옵니다.
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-// ejs 설치 및 뷰 엔진 설정
-app.set("view engine", "ejs");
-
-// uuid를 이용해 id 생성
 app.get("/", (req, res) => {
-  res.redirect(`${uuidv4()}`);
+  res.redirect(`/${uuidV4()}`);
 });
 
-// id를 room.ejs에 전달
 app.get("/:room", (req, res) => {
   res.render("room", { roomId: req.params.room });
 });
 
 io.on("connection", (socket) => {
-  socket.on("join-room", (roomId) => {
+  socket.on("join-room", (roomId, userId) => {
     socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
 
-    socket.to(roomId).broadcast.emit("user-connected");
+    socket.on("disconnect", () => {
+      socket.to(roomId).broadcast.emit("user-disconnected", userId);
+    });
   });
 });
 
-server.listen(3030);
-
-// io.sokets.emit, socket.broadcast.emit 차이
-
-// broadcast.emit에서 계속해서 오류가 생겼습니다. 이유는 socket.io의 버전 문제였습니다. 삭제한후 버전을 바꾸어서 해결했습니다.
+server.listen(3000);
